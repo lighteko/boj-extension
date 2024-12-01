18909a0c4cbb43fd2e2554802cfaf4b612477fd7
lighteko <dev.lighteko@gmail.com>
2024-12-01 10:56:24 -0500

    final revision

--- content.js
+++ content.js
@@ -2,15 +2,21 @@ window.onload = async () => {
   console.log("BOJ-EXTENSION CALLED");
   const table = document.querySelector(".table").childNodes;
   const header = table[0].childNodes[0];
-  header.childNodes[0].style = "width: 6%";
-  header.childNodes[1].style = "width: 49%";
+  header.childNodes[0].style = "width: 7%";
+  header.childNodes[1].style = "width: 48%";
   header.childNodes[3].style = "width: 7%";
   const body = table[1].childNodes;
   const problems = [];
 
-  for (let tr of body) {
-    const pID = tr.querySelector(".list_problem_id").innerText;
-    problems.push(pID);
+  if (window.location.pathname.includes("/problem/")) {
+    const url = window.location.pathname.split("/");
+    problems.push(url[url.length - 1]);
+    console.log(problems);
+  } else {
+    for (let tr of body) {
+      const pID = tr.childNodes[0].innerText;
+      problems.push(pID);
+    }
   }
 
   const res = await fetch(chrome.runtime.getURL("problems.json"));
@@ -23,7 +29,7 @@ window.onload = async () => {
     const tr = body[i];
     const level = levels[problems[i]];
     const tierBody = document.createElement("td");
-    tierBody.innerHTML = `<img src="https://static.solved.ac/tier_small/${level}.svg" alt="${level}-level" />`;
+    tierBody.innerHTML = `<img style="width: 50%" src="https://static.solved.ac/tier_small/${level}.svg" alt="${level}-level" />`;
     tr.insertBefore(tierBody, tr.childNodes[0]);
   }
 };
